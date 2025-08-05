fea47b99cf01f62d8ea9d152f2a91bb113e39975
Fabrici01 <fabricioamaral504@gmail.com>
2025-08-05 13:49:16 -0300

    v2

--- main.c
+++ main.c
@@ -1,127 +1,86 @@
 #include <stdio.h>
 #include <stdlib.h>
+
 #define INFINITO 99999999
 
-typedef struct noLista{
-    int info;
-    struct noLista *prox;
-}NoLista;
+int minimaDistancia(int distancia[], int vertices, int visitado[]);
 
-void criarLista(NoLista** l);
-int estaVazia(NoLista** l);
-void inserirElemento(NoLista** l, int v);
-void removerElemento(NoLista** l, int v);
-void liberarLista(NoLista** l);
-int numeroElementos(NoLista** l);
-
-int main() {
+int main(int argc, char *argv[]) {
     int vertices = 0, arestas = 0;
+    
     scanf("%d %d", &vertices, &arestas);
-    int grafo[vertices][vertices];
+    
+    int **grafo = malloc(vertices * sizeof(int *));
     for (int i = 0; i < vertices; i++) {
-        for (int j = 0; j < vertices; j++) {
-            grafo[i][j] = 0;
-        }
+        grafo[i] = calloc(vertices, sizeof(int));
     }
+    
     for (int i = 0; i < arestas; i++) {
         int u, v, w;
         scanf("%d %d %d", &u, &v, &w);
-        grafo[u-1][v-1] = w;
-        grafo[v-1][u-1] = w;
+        grafo[u - 1][v - 1] = w;
+        grafo[v - 1][u - 1] = w;
     }
-    int origem, destino, proibido;
-    scanf("%d %d %d", &origem, &destino, &proibido);
-    //Algoritmo de Dijkstra
-    NoLista* lista;
-    criarLista(&lista);
-    for (int i = 0; i < vertices; i++)
-    {
-        if (i+1 != origem) {
-            if (i+1 == destino)
-            {
-                inserirElemento(&lista, i+1);
-            }
-            else if (i+1 != vertices-proibido+i)
-            {
-                inserirElemento(&lista, i+1);
+    
+    int origem, destino, num_proibido;
+    scanf("%d %d %d", &origem, &destino, &num_proibido);
+    //Algoritmo Dijkstra
+    int predecessor[vertices], distancia[vertices], visitado[vertices];
+    
+    for (int i = 0; i < vertices; i++) {
+        predecessor[i] = -1;
+        distancia[i] = INFINITO;
+        visitado[i] = 0;
+    }
+    
+    for (int i = 0; i < vertices; i++) {
+        if ((i + 1 > vertices - num_proibido) && (i + 1 != destino) && (i+1 != origem)) {
+            visitado[i] = 1;
+        }
+    }
+    
+    distancia[origem - 1] = 0;
+    
+    for (int i = 0; i < vertices; i++) {
+        int u = minimaDistancia(distancia, vertices, visitado);
+        
+        if (u == -1) {
+            break;
+        }
+        
+        visitado[u] = 1;
+        //Hora de Relaxar
+        for (int v = 0; v < vertices; v++) {
+            if (!visitado[v] && grafo[u][v] && distancia[u] != INFINITO && distancia[u] + grafo[u][v] < distancia[v]) {
+                distancia[v] = distancia[u] + grafo[u][v];
+                predecessor[v] = u;
             }
         }
     }
-    int predecessor[vertices], distancia[vertices];
-    for (int i = 0; i < vertices; i++)
-    {
-        distancia[i] = INFINITO;
-        predecessor[i] = -1;
+    //Resultado
+    if (distancia[destino - 1] == INFINITO) {
+        printf("YL nao podera realizar esta viagem\n");
+    } else {
+        printf("%d\n", distancia[destino - 1]);
     }
-    distancia[origem-1] = 0;
-    while (!estaVazia(&lista)) {
-        int v = minimo(&distancia, vertices);
+    
+    for (int i = 0; i < vertices; i++) {
+        free(grafo[i]);
     }
+    free(grafo);
+    
     return 0;
 }
 
-void criarLista(NoLista** l){
-    *l = NULL;
-}
-
-int estaVazia(NoLista** l){
-    return (*l == NULL);
-}
-
-void inserirElemento(NoLista** l, int v){
-    NoLista* novo=(NoLista*)malloc(sizeof(NoLista));
-    if(novo!=NULL){
-        novo->info=v;
-        novo->prox=*l;
-        *l = novo;
-    }
-    else{
-        printf("Nao foi possivel alocar espaço!\n");
-        exit(1);
-    }
-}
-
-void removerElemento(NoLista** l, int v){
-    NoLista *ant=NULL, *p;
-    if(!estaVazia(l)){
-        for(p=*l; p!=NULL && p->info!=v; p=p->prox){
-            ant=p;
+int minimaDistancia(int distancia[], int vertices, int visitado[]) {
+    int min = INFINITO;
+    int i = -1;
+    
+    for (int j = 0; j < vertices; j++) {
+        if (visitado[j] == 0 && distancia[j] <= min) {
+            min = distancia[j];
+            i = j;
         }
-        if(p==NULL)
-            printf("Elemento nao encontrado!\n");
-        else{
-            if(ant==NULL)
-                *l=p->prox;
-            else
-                ant->prox = p->prox;
-            free(p);
-        }
-    }
-}
-
-void liberarLista(NoLista** l){
-    NoLista *p, *temp;
-    for(p=*l; p!=NULL; p=temp){
-        temp = p->prox;
-        free(p);
-    }
-}
-
-int numeroElementos(NoLista** l){
-    NoLista *p;
-    int i=0;
-    for(p=*l; p!=NULL; p=p->prox){
-        i++;
     }
     return i;
-}
-
-int minimo (int *vetor, int tamanho) {
-    int min = INFINITO;
-    for (int i = 0; i < tamanho; i++) {
-        if (vetor[i] < min && vetor[i] != 0) {
-            min = vetor[i];
-        }
-    }
-    return min;
 }
